7365f8409f5b45b14dc981f66dff0d77584a230b
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-11-07 14:59:43 +0700

    feat: initial commit

---
+++ src/env.mjs
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
+  },
+  server: {
+    OPENROUTER_URL: z.string().min(1),
+    OPENROUTER_API_KEY: z.string().min(1),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    OPENROUTER_URL: process.env.OPENROUTER_URL,
+    OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
+  },
+})
