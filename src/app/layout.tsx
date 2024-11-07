09247bca00df4a6d3f67d4d07b019aadad09a0d0
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-11-07 15:07:27 +0700

    feat: theming font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import "@/styles/globals.css"
 
 import type { Metadata, Viewport } from "next"
-import { Inter } from "next/font/google"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
-
-const inter = Inter({ subsets: ["latin"] })
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -67,12 +64,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background antialiased",
-          inter.className
-        )}
-      >
+      <body className={cn("min-h-screen antialiased")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
